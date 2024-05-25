cf1bb3b89055fac1aa950cbcbe406fc5bccddcc2
wingsdh <wingsdh@gmail.com>
2024-05-25 12:26:32 +0800

    use copilot-lua-cmp instead of copilot-lua

--- lua/community.lua
+++ lua/community.lua
@@ -15,5 +15,6 @@ return {
   { import = "astrocommunity/pack/python" },
   { import = "astrocommunity/syntax/vim-cool" },
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity/completion/copilot-lua-cmp" },
   -- import/override with your plugins folder
 }
